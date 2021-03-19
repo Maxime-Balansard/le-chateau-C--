6505e2e8cfea52b51c4a5758ce23965ea15aae17
Maxime-Balansard <72392192+Maxime-Balansard@users.noreply.github.com>
2021-03-19 09:11:28 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include "Personnage.h"
 #include "chevalier_de_la_rosette.h"
-
+#include <random>
+#include <ctime>
 using namespace std;
+
+//vector<string>  tabCroyance = {"Mezoreth","Luminark","Shadark","Pyriis"};
 
 int main()
 {
+    srand(time(NULL));
     cout << "Hello world!" << endl;
 
-    Personnage p1("Jean", 5, 5, 3, 0, 0.05, 0.05, 0.05, 10, 1, Mezoreth);
+    Personnage p1("Jean", 5, 5, -5, 51, 0.05, 0.05, 0.05, -10, 6, -1);
     chevalier_de_la_rosette p2;
-    chevalier_de_la_rosette p3("Jean", 15, 5, 3, 0, 0.05, 0.05, 0.05, 10, 1, Mezoreth);
+    chevalier_de_la_rosette p3("Jeannette", 15, 5, 3, 0, 0.05, 0.05, 0.05, 10, 1, 0);
     p1.fiche();
-    p2.fiche();
-    p3.fiche();
+    //p2.fiche();
+    //p3.fiche();
 
     return 0;
 }
